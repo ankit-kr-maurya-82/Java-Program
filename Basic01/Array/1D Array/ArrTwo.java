f260d6868299deb10af6183b424677a8884ec6c9
AK Maurya <143257696+ankit-kr-maurya-82@users.noreply.github.com>
2026-01-18 13:30:07 +0530

    DSA Solution

--- Basic01/Array/1D Array/ArrTwo.java
+++ Basic01/Array/1D Array/ArrTwo.java
@@ -5,12 +5,12 @@ public class ArrTwo {
 
     public static void main(String[] args) {
 
-        System.out.println("Input size of array: ");
+        System.out.print("Input size of array: ");
         Scanner sc = new Scanner(System.in);
         int size = sc.nextInt();
         int numbers[] = new int[size];
 
-        System.out.println("Enter the number: ");
+        System.out.print("Enter the number: ");
         // input
         for (int i = 0; i < size; i++) {
             numbers[i] = sc.nextInt();
@@ -22,11 +22,11 @@ public class ArrTwo {
         }
 
         // Linear Search
-        System.out.println("Find the number: ");
+        System.out.print("Find the number: ");
         int x = sc.nextInt();
         for (int i = 0; i < numbers.length; i++) {
             if(numbers[i] == x){
-                System.out.println("x found at index : "+ i);
+                System.out.print("x found at index : "+ i);
             }
         }
 
