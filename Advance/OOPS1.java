d1503bb24c2623f054b2575d0a4e2c84c6c8eb0f
ankit-kr-maurya <ankitkrmaurya82@gmail.com>
2026-07-12 10:10:28 +0530

    OOP

--- Advance/OOPS1.java
+++ Advance/OOPS1.java
@@ -1,16 +1,12 @@
-
-
-
-
-class Pen {
+class Pen{
     String color;
-    String type;
+    String type; // ballpoint; gel
 
-    public  void write() {
-        System.err.println("writing something");
+    public void write() {
+        System.out.println("writing something");
     }
 
-    public void printColor() {
+    public void printColor(){
         System.out.println(this.color);
     }
 }
@@ -19,46 +15,15 @@ class Student {
     String name;
     int age;
 
-    public void printInfo() {
+    public void printInfo(){
         System.out.println(this.name);
         System.out.println(this.age);
     }
-
-    Student(String name, int age) {
-        this.name = name;
-        this.age = age;
-    }
-
-    public void printInfo(String name){
-        System.out.println(name);
-    }
-
-    public void printInfo(int age){
-        System.out.println(age);
-    }
-
-    public void printInfo(String name, int age){
-        System.out.println(name + " " + age);
-    }
-
-    Student(Student s2) {
-        this.name = s2.name;
-        this.age = s2.age;
-    }
-
-    Student() {
-
-    }
-
-    
-    
-
-    
 }
 
-
-public class OOPS1 {
+public class OOPS1{
     public static void main(String[] args) {
+        // Pen
         Pen pen1 = new Pen();
         pen1.color = "blue";
         pen1.type = "gel";
@@ -71,15 +36,14 @@ public class OOPS1 {
 
         pen1.printColor();
         pen2.printColor();
+        String star = "*";
+        System.out.println(star.repeat(12));
 
+        // student
+        Student s1 = new Student();
+        s1.name = "Jatin";
+        s1.age = 18;
 
-        Student s1 = new Student("ankit", 12);
-        // s1.name = "aman";
-        // s1.age = 21;
-        Student s2 = new Student(s1);
-        s2.printInfo();
-        s1.printInfo(s1.age);
-        s1.printInfo(s1.name);
-        s1.printInfo(s1.name,s1.age);
+        s1.printInfo();
     }
 }
