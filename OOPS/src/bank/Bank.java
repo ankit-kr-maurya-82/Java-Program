45f427d7b68ee0ee20864f25fe98f6b5fb4d55ae
ankit-kr-maurya <ankitkrmaurya82@gmail.com>
2026-07-13 09:37:47 +0530

    Getter and Setter

--- OOPS/src/bank/Bank.java
+++ OOPS/src/bank/Bank.java
@@ -1,9 +1,26 @@
-package bank;
+// package bank;
 
 class Account{
     public String name;
+    protected String email;
+    private String password;
+
+    // getters
+    public String getPassword(){
+        return this.password;
+    }
+
+    public void setPassword(String pass){
+        this.password = pass;
+    }
 }
 
 public class Bank{
-    
+    public void main(){
+        Account account1 = new Account();
+        account1.name = "Ankit";
+        account1.email = "ankit@gmail.com";
+        account1.setPassword("abcd");
+        System.out.println(account1.getPassword());
+    }
 }
