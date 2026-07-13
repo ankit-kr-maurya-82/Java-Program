bd59b09a1aeb250d94895158fd32dfc4920f6984
ankit-kr-maurya <ankitkrmaurya82@gmail.com>
2026-07-13 10:01:20 +0530

    interface

---
+++ OOPS/Interface.java
@@ -0,0 +1,16 @@
+interface Animal{
+    public void walk();
+}
+
+class Horse implements Animal{
+    public void walk(){
+        System.out.println("walks on 4 legs");
+    }
+}
+
+class Interface{
+    public static void main(String[] args) {
+        Horse horse = new Horse();
+        horse.walk();
+    }
+}
