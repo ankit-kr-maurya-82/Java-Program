532be0786feb143c3d3b73e8c823491a31bdce29
ankit-kr-maurya <ankitkrmaurya82@gmail.com>
2026-07-25 13:51:39 +0530

    update array

--- Basic01/Array/1D Array/ArrFour.java
+++ Basic01/Array/1D Array/ArrFour.java
@@ -6,11 +6,14 @@ import java.util.Scanner;
 class ArrFour{
 
    public static int delete(int arr[], int size, int pos){
-        for(int i = pos -1; i< size-1; i++){
+        for(int i=pos-1; i<size-1; i++){
             arr[i] = arr[i+1];
         }
 
         return size -1;
+    }
+    public static void update(int arr[], int size, int pos, int value) {
+        arr[pos-1]=value;
     }
 
 
@@ -37,7 +40,7 @@ class ArrFour{
         System.out.println("Enter position to delete: ");
         int pos = sc.nextInt();
 
-        if(pos < 1 || pos >size){
+        if(pos < 1 || pos > size){
             System.out.println("Invalid");
         }else{
            size = delete(num, size, pos);
@@ -45,8 +48,28 @@ class ArrFour{
             System.out.println("Array after deletion: ");
             for (int i = 0; i < size; i++) {
             System.out.print(num[i] + " ");
+            }
         }
+
+        // update
+        System.out.println("\nEnter position to update: ");
+        pos = sc.nextInt();
+
+        if(pos < 1 || pos > size){
+            System.out.println("Invalid");
+        }else{
+           System.out.print("Enter value: ");
+           int value = sc.nextInt();
+
+           update(num, size, pos, value);
+
+           System.err.println("Array after update");
+
+           for (int i = 0; i < size; i++) {
+                System.out.print(num[i] + " ");
+            }
         }
+
 
 
         
