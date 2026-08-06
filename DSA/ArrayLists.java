0df34c68641db5db64eb113a062a21b9f9686cc8
ankit-kr-maurya <ankitkrmaurya82@gmail.com>
2026-08-06 22:53:05 +0530

    arraylist

--- DSA/ArrayLists.java
+++ DSA/ArrayLists.java
@@ -1,47 +1,31 @@
-import  java.util.ArrayList;
-import java.util.Collections;
+import java.util.ArrayList;
 
 class ArrayLists{
     public static void main(String[] args) {
-        ArrayList<Integer> list = new ArrayList<Integer>();
+        ArrayList<Integer> list1 = new ArrayList<Integer>();
+        ArrayList<String> fruits = new ArrayList<String>();
 
         // add elements
-        list.add(0);
-        list.add(2);
-        list.add(3);
-        System.out.println(list);
+        fruits.add("Apple");
+        fruits.add("Banana");
+        fruits.add("Cherry");
+        System.out.println(fruits);
 
         // get elements
-        int element = list.get(0);
-        System.out.println(element);
 
         // add el in between
 
-        list.add(1,1);
-        System.out.println(list);
 
         // set element
-        list.set(0,5);
-        System.out.println(list);
 
         // delete element
-        list.remove(3);
-        System.out.println(list);
 
         // size 
-        int size = list.size();
-        System.out.println(size);
 
         // loop
 
-        for(int i=0; i<list.size(); i++){
-            System.out.print(list.get(i));
-        }
-        System.out.println();
 
         // sorting
-        Collections.sort(list);
-        System.out.println(list);
     }
     
 }
